5bf9b3bcb31335d99b829d0cb6c698d52283e3aa
Ezzerland <Ezzerland@gmail.com>
2019-06-05 19:41:08 -0700

    Whole 1 line of code needed for plugin

---
+++ src/ezzerland/ravenloftmc/instantitempickup.java
@@ -0,0 +1,14 @@
+package ezzerland.ravenloftmc;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.ItemSpawnEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class instantitempickup extends JavaPlugin implements Listener
+{
+  // When an item spawns, set it's pick up delay to 0.
+  @EventHandler(ignoreCancelled = true, priority = EventPriority.HIGHEST)
+  public void onItemSpawn(ItemSpawnEvent e) { e.getEntity().setPickupDelay(0); }
+}

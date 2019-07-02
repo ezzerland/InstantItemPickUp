f956b140996fdec79c9f0044631ebd6d6d4fdcd7
Ezzerland <Ezzerland@gmail.com>
2019-07-02 15:10:51 -0700

    Register the event.. you know, so it works xD

--- src/ezzerland/ravenloftmc/instantitempickup.java
+++ src/ezzerland/ravenloftmc/instantitempickup.java
@@ -8,6 +8,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class instantitempickup extends JavaPlugin implements Listener
 {
+  public void onEnable() { getServer().getPluginManager().registerEvents(this, this); }
   // When an item spawns, set it's pick up delay to 0.
   @EventHandler(ignoreCancelled = true, priority = EventPriority.HIGHEST)
   public void onItemSpawn(ItemSpawnEvent e) { e.getEntity().setPickupDelay(0); }

801a403ee63e0d916a70b7aa0641e5f5e22e7ac7
Ezzerland <Ezzerland@gmail.com>
2019-07-02 15:40:03 -0700

    Fix issue of player being unable to drop items because they instantly pick them back up

--- src/ezzerland/ravenloftmc/instantitempickup.java
+++ src/ezzerland/ravenloftmc/instantitempickup.java
@@ -1,15 +1,33 @@
 package ezzerland.ravenloftmc;
 
+import java.util.List;
+
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.ItemSpawnEvent;
+import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class instantitempickup extends JavaPlugin implements Listener
 {
   public void onEnable() { getServer().getPluginManager().registerEvents(this, this); }
-  // When an item spawns, set it's pick up delay to 0.
+  public List<Integer> ignore;
+  
   @EventHandler(ignoreCancelled = true, priority = EventPriority.HIGHEST)
-  public void onItemSpawn(ItemSpawnEvent e) { e.getEntity().setPickupDelay(0); }
+  public void onPlayerDropItem (PlayerDropItemEvent e)
+  { // If player drops item, add to ignore list, otherwise player can't drop item
+    ignore.add(e.getItemDrop().getEntityId());
+  }
+  
+  @EventHandler(ignoreCancelled = true, priority = EventPriority.HIGHEST)
+  public void onItemSpawn(ItemSpawnEvent e)
+  { // When an item spawns, set it's pick up delay to 0, unless we should ignore it
+    if (ignore.contains(e.getEntity().getEntityId()))
+    {
+      ignore.remove(e.getEntity().getEntityId());
+      return;
+    }
+    e.getEntity().setPickupDelay(0);
+  }
 }
